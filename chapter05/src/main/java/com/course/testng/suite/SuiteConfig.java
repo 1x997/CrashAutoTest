52ebb9b976613a45b69356c2599322dc930ee918
1x997 <1620748113@qq.com>
2024-08-26 14:06:51 +0800

    testng

---
+++ chapter05/src/main/java/com/course/testng/suite/SuiteConfig.java
@@ -0,0 +1,10 @@
+package com.course.testng.suite;
+
+public class SuiteConfig {
+    public void beforeSuit(){
+        System.out.println("before suite运行啦");
+    }
+    public void afterSuit(){
+        System.out.println("after suite运行啦");
+    }
+}
